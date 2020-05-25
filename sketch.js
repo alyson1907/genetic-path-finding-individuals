655d43555687ea57c66a9f25a8e3727f16b39d2c
Alyson Matheus <alyson1907@gmail.com>
2020-05-25 20:03:31 -0300

    +Author name & basic stylization for body

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1080, 700)
+  background(50)
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    fill(0)
   } else {
-    fill(255);
+    fill(255)
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 80, 80)
 }
